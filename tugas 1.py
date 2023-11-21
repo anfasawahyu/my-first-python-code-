2e325276c19c0c363c7d5b5d12990b79dcdae97b
fasa <anfasawahyu@gmail.com>
2023-11-21 17:51:11 +0700

    Tugas Pertama

---
+++ tugas 1.py
@@ -0,0 +1,13 @@
+"""
+Tugas Pertama
+"""
+print('Ibu memberi perintah "Beli 1 botol susu"')
+print('Budi menjawab "oke"')
+print("budi pergi ke toko")
+print('apakah susu ada? "ada"')
+print('budi mengecek harganya, cukup')
+print('apakah ada telur? "tidak"')
+print('budi membeli satu botol susu')
+print('budi pulang kerumah')
+print('budi menyerahkan hasil belanjanya kepada ibu')
+print('Happy Ending')
