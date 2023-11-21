8c932f0c9eb3dfc59b32412554e643136d2b6d67
fasa <anfasawahyu@gmail.com>
2023-11-21 20:16:45 +0700

    Tugas Pertama

--- tugas 1.py
+++ tugas 1.py
@@ -1,17 +1,23 @@
 """
 Tugas Pertama
 """
-print('Ibu memberi perintah "Beli 1 botol susu"')
+print('Ibu memberi perintah "Beli 1 botol susu apabila ada telur maka beli juga"')
 print('Budi menjawab "oke"')
 print("budi pergi ke toko")
+print('budi mulai berbelanja di toko')
 
 jumlah_botol_susu = 10
+jumlah_butir_telur = 100
+print(f'jumlah botol susu {jumlah_botol_susu} Botol')
+print(f'jumlah butir telur {jumlah_butir_telur} butir')
 
 if jumlah_botol_susu > 0 :
-    print('budi mengecek harganya, dan ternyata uangnya cukup untuk beli')
     print('Budi membeli 1 botol susu')
+if jumlah_butir_telur > 0 :
+    print('budi membeli 6 butir telur')
 else :
     print('budi tidak membeli botol susu')
+    print('budi tidak membeli telur')
 
 print('budi pulang kerumah')
 print('budi menyerahkan hasil belanjanya kepada ibu')
